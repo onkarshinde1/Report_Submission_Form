6cb00b0f6a5b88761c70e0bd9a0e12c95a9214ef
onkarshinde1 <122793973+onkarshinde1@users.noreply.github.com>
2023-02-08 15:57:14 +0530

    Add files via upload
    here the report submission form using html, css and php.

---
+++ data.php
@@ -0,0 +1,8 @@
+
+
+full-name <?php echo $_POST["fullname"]; ?><br>
+Student-ID <?php echo $_POST["student_ID"]; ?><br>
+email : <?php echo $_POST["email"];?><br>
+Program : <?php echo $_POST['program']?><br>
+Semester is :<?php echo $_POST['semester']?><br>
+Title of project is : <?php echo $_POST['project_title']?><br>
